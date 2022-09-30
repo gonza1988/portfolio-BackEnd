c083965bf625fb0d810de9d5dc7e79a65fdbe19a
Gonzalo Cozzo <gonza007_88@hotmail.com>
2022-09-30 19:08:01 -0300

    cambiando rutas a heroku

--- src/main/java/com/portfolio/cozzo/Controller/CEducacion.java
+++ src/main/java/com/portfolio/cozzo/Controller/CEducacion.java
@@ -26,8 +26,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/educacion")
-@CrossOrigin(origins = "http://localhost:4200")
-//@CrossOrigin(origins = "https://frontendcozzo.web.app")
+//@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://frontendcozzo.web.app")
 public class CEducacion {
     
     @Autowired
